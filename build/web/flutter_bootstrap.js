e98166f62fb2fc5d364b6e16b3b46d30c9774271
Favour Chigoziri Akubuo <akubuof.work@gmail.com>
2025-10-12 13:14:24 +0200

    Update vercel config and service worker assets
    Added custom cache-control headers for static assets in vercel.json and updated routing to serve index.html from the root. Also updated service worker and bootstrap asset hashes to reflect new build outputs.

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3592596431"
+    serviceWorkerVersion: "3497170192"
   }
 });
